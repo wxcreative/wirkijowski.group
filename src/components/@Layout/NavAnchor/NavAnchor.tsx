464b834586ade13774f28b8b800d7c5eb861f6ea
Sebastian Wirkijowski <sebastian@wirkijowski.dev>
2025-02-20 16:32:19 +0100

    refactor: removed button from `NavAnchor` component, use only anchor instead.

--- src/components/@Layout/NavAnchor/NavAnchor.tsx
+++ src/components/@Layout/NavAnchor/NavAnchor.tsx
@@ -3,7 +3,7 @@ import {ReactNode} from "react";
 import Link from "next/link";
 import {cx} from "@utils/cx";
 
-import {IconExternal} from "@comp/NavAnchor/IconExternal";
+import {IconExternal} from "@comp/Icon/IconExternal";
 
 interface INavButtonProps {
 	href: string
@@ -14,14 +14,12 @@ interface INavButtonProps {
 export function NavAnchor ({href, external = false, children}: INavButtonProps): ReactNode {
 	return (
 		<li className={cx('py-2 list-none flex flex-row')}>
-			<Link passHref href={href}>
-				<button className={cx(
+			<Link href={href} className={cx(
 					"transition h-fit font-medium text-sm/8 flex flex-row gap-1 px-2 items-center cursor-pointer",
 					"hover:bg-accent-gray-2 rounded-sm"
-				)}>
-					{children}
-					{external && <IconExternal />}
-				</button>
+			)}>
+				{children}
+				{external && <IconExternal />}
 			</Link>
 		</li>
 	)
